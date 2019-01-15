1d8d186110839e3dd06a174afe267af6523562ec
hal <dimmehalatsis96@gmail.com>
2019-01-15 04:39:47 +0200

    added functions

--- tweets.cpp
+++ tweets.cpp
@@ -1,30 +1,55 @@
 #include "tweets.h"
 
+#include <sstream> 
+
 
 
 using vector_2d = std::vector<std::vector<std::string>> ;
 
 void tweets::process_tweets(const vector_2d& vref) {
 
-  crypto_id = new uint32_t[vref.size()];
+  uint64_t tid;
   for (int i=0; i < vref.size(); i++) {
-    for (int j=0; j < vref[i].size(); j++) {
+    //2nd column are the tweet ids
+    std::stringstream s(vref[i][1]);
+    s >> tid;
+    crypto_id[tid] = -1;
+
+    for (int j=2; j < vref[i].size(); j++) {
       if (cc_id.find(vref[i][j]) != cc_id.end()) {
-        crypto_id[cc_id[vref[i][j]]] = j;
+        crypto_id[tid] = cc_id[vref[i][j]];
+        break;
       }
+    }
+
+  }
+  //calculate the sentimental value for each tweet
+  for (int i=0; i < vref.size(); i++) {
+    double sum = 0.0;
+    for (int j=2; j < vref[i].size(); j++) {
+      sum += (lexicon.find(vref[i][j]) != lexicon.end())?
+        lexicon[vref[i][j]]:0.0;
     }
 
   }
 }
 
-
+//learn to what cryptocurrency does each term correspond to
 void tweets::process_crypto_terms(const vector_2d& vref) {
 
   for (int i=0;i < vref.size(); i++) {
-    crypto_pos[vref[i][0]] = i;
     for (int j=0; j < vref[i].size(); j++) {
       cc_id[vref[i][j]] = i;
-
     }
+  }
+}
+
+
+void tweets::process_dict(const vector_2d& vref) {
+  double score;
+  for (auto& mapping: vref) {
+    std::stringstream s(mapping[1]);
+    s >> score;
+    lexicon[mapping[0]] = score;
   }
 }
--- tweets.h
+++ tweets.h
@@ -15,23 +15,38 @@ class tweets {
 
   public:
   tweets() {
-    crypto_id = nullptr;
+    ;
   }
 
   void process_tweets(const std::vector<std::vector<std::string>>&);
   void process_crypto_terms(const std::vector<std::vector<std::string>>&);
+  void process_dict(const std::vector<std::vector<std::string>>&);
   
   std::vector<std::vector<double>> generate_ujs();
 
+  /*
+  @crypto_pos , maps a cryptocurrencies string to an id
+  ids are the implicit index derived from the line in the file
+  @cc_id maps a term to a cryptocurrency id
+  @tweet_score , maps a tweet id to it's score (sum of terms)
+  //TODO if the crypt score contains values in a full integer range
+  maybe use array?
+
+  @crypto_id maps each tweet_id to a cryptocurrency id;
+  */
+
+
+
+  //maps every cryptocurrecy to an id
+
   
+  std::unordered_map<std::string , int64_t> crypto_pos;
+  std::unordered_map<std::string, int64_t> cc_id;
+  //double* tweet_score;
+  std::unordered_map<uint64_t, double> crypt_score;
+  std::unordered_map<uint64_t, int64_t> crypto_id;
 
-
-
-    //maps every cryptocurrecy to an id
-  std::unordered_map<std::string , int> crypto_pos;
-  std::unordered_map<std::string, int> cc_id;
-  double* tweet_score;
-  uint32_t* crypto_id;
+  std::unordered_map<std::string, double> lexicon;
 };
 
 #endif
